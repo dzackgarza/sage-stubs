922c9aa302ebea4f09ecf4e8754941962fdad733
Zack Garza <dzackgarza@gmail.com>
2026-08-15 20:33:10 +0800

    Consolidate remaining Sage stubs

--- sage-stubs/rings/complex_conversion.pyi
+++ sage-stubs/rings/complex_conversion.pyi
@@ -2,5 +2,5 @@ from sage.categories.map import Map
 from sage.rings.complex_double import ComplexDoubleElement
 from sage.rings.complex_mpfr import ComplexNumber
 
-class CCtoCDF(Map):
+class CCtoCDF(Map[ComplexNumber, ComplexDoubleElement]):
     def _call_(self, x: ComplexNumber) -> ComplexDoubleElement: ...
