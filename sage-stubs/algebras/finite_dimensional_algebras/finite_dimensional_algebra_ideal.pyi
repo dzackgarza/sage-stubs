e580c90710b4376defdcfd7c27750004e510163f
D. Zack Garza <dzackgarza@gmail.com>
2026-08-17 01:22:47 +0800

    feat(stubs): model finite-dimensional algebras over fields

--- sage-stubs/algebras/finite_dimensional_algebras/finite_dimensional_algebra_ideal.pyi
+++ sage-stubs/algebras/finite_dimensional_algebras/finite_dimensional_algebra_ideal.pyi
@@ -1,8 +1,38 @@
-from sage.structure.element import Element
+from typing import Generic, Self, TypeVar
 
-class FiniteDimensionalAlgebraIdeal(Ideal_generic):
-    def _richcmp_(self, other: Element, op: Element | int | str | bool | None) -> Element: ...
-    def basis_matrix(self) -> Element: ...
-    def vector_space(self) -> Element: ...
+from sage.algebras.finite_dimensional_algebras.finite_dimensional_algebra import (
+    FiniteDimensionalAlgebra,
+    FiniteDimensionalIdealInput,
+)
+from sage.algebras.finite_dimensional_algebras.finite_dimensional_algebra_element import (
+    FiniteDimensionalAlgebraElement,
+)
+from sage.matrix.matrix0 import Matrix
+from sage.modules.free_module import FreeModule_generic
+from sage.rings.ideal import Ideal_generic
+from sage.structure.element import FieldElement
 
-class Ideal_generic: ...
+_Scalar = TypeVar(
+    "_Scalar",
+    bound=FieldElement,
+    default=FieldElement,
+)
+
+class FiniteDimensionalAlgebraIdeal(
+    Ideal_generic,
+    Generic[_Scalar],
+):
+    def __init__(
+        self,
+        A: FiniteDimensionalAlgebra[_Scalar],
+        gens: FiniteDimensionalIdealInput[_Scalar] = None,
+        given_by_matrix: bool = False,
+    ) -> None: ...
+    def ring(self) -> FiniteDimensionalAlgebra[_Scalar]: ...
+    def _richcmp_(self, other: Self, op: int) -> bool: ...
+    def __contains__(
+        self,
+        elt: FiniteDimensionalAlgebraElement[_Scalar] | object,
+    ) -> bool: ...
+    def basis_matrix(self) -> Matrix[_Scalar]: ...
+    def vector_space(self) -> FreeModule_generic[_Scalar]: ...
--- sage-stubs/algebras/finite_dimensional_algebras/finite_dimensional_algebra_morphism.pyi
+++ sage-stubs/algebras/finite_dimensional_algebras/finite_dimensional_algebra_morphism.pyi
@@ -1,13 +1,80 @@
-from sage.structure.element import Element
+from collections.abc import Sequence
+from typing import Generic, TypeVar, overload
 
-class FiniteDimensionalAlgebraMorphism(RingHomomorphism_im_gens):
+from sage.algebras.finite_dimensional_algebras.finite_dimensional_algebra import (
+    FiniteDimensionalAlgebra,
+    FiniteDimensionalAlgebraElementInput,
+)
+from sage.algebras.finite_dimensional_algebras.finite_dimensional_algebra_element import (
+    FiniteDimensionalAlgebraElement,
+)
+from sage.algebras.finite_dimensional_algebras.finite_dimensional_algebra_ideal import (
+    FiniteDimensionalAlgebraIdeal,
+)
+from sage.matrix.matrix0 import Matrix
+from sage.rings.homset import RingHomset_generic
+from sage.rings.morphism import RingHomomorphism_im_gens
+from sage.structure.element import FieldElement
+
+_Scalar = TypeVar(
+    "_Scalar",
+    bound=FieldElement,
+    default=FieldElement,
+)
+
+type FiniteDimensionalLinearMapInput[_Scalar: FieldElement] = (
+    Matrix[_Scalar]
+    | Sequence[Sequence[_Scalar | int]]
+)
+
+class FiniteDimensionalAlgebraMorphism(
+    RingHomomorphism_im_gens,
+    Generic[_Scalar],
+):
+    def __init__(
+        self,
+        parent: FiniteDimensionalAlgebraHomset[_Scalar],
+        f: Matrix[_Scalar],
+        check: bool = True,
+        unitary: bool = True,
+    ) -> None: ...
+    def parent(self) -> FiniteDimensionalAlgebraHomset[_Scalar]: ...
+    def domain(self) -> FiniteDimensionalAlgebra[_Scalar]: ...
+    def codomain(self) -> FiniteDimensionalAlgebra[_Scalar]: ...
     def _repr_(self) -> str: ...
-    def matrix(self) -> Element: ...
-    def inverse_image(self, I: Element | int | str | bool | None) -> Element: ...
+    def __call__(
+        self,
+        x: FiniteDimensionalAlgebraElementInput[_Scalar],
+    ) -> FiniteDimensionalAlgebraElement[_Scalar]: ...
+    def __eq__(self, other: object) -> bool: ...
+    def __ne__(self, other: object) -> bool: ...
+    def matrix(self) -> Matrix[_Scalar]: ...
+    def inverse_image(
+        self,
+        I: FiniteDimensionalAlgebraIdeal[_Scalar],
+    ) -> FiniteDimensionalAlgebraIdeal[_Scalar]: ...
 
-class FiniteDimensionalAlgebraHomset(RingHomset_generic):
-    def zero(self) -> Element: ...
-
-class RingHomomorphism_im_gens: ...
-
-class RingHomset_generic: ...
+class FiniteDimensionalAlgebraHomset(
+    RingHomset_generic[
+        FiniteDimensionalAlgebraElement[_Scalar],
+        FiniteDimensionalAlgebraElement[_Scalar],
+    ],
+    Generic[_Scalar],
+):
+    def domain(self) -> FiniteDimensionalAlgebra[_Scalar]: ...
+    def codomain(self) -> FiniteDimensionalAlgebra[_Scalar]: ...
+    def zero(self) -> FiniteDimensionalAlgebraMorphism[_Scalar]: ...
+    @overload
+    def __call__(
+        self,
+        f: FiniteDimensionalAlgebraMorphism[_Scalar],
+        check: bool = True,
+        unitary: bool = True,
+    ) -> FiniteDimensionalAlgebraMorphism[_Scalar]: ...
+    @overload
+    def __call__(
+        self,
+        f: FiniteDimensionalLinearMapInput[_Scalar],
+        check: bool = True,
+        unitary: bool = True,
+    ) -> FiniteDimensionalAlgebraMorphism[_Scalar]: ...
