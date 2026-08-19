61073743508e6d4237f89a59d2c0ebc2bb5a2dec
D. Zack Garza <dzackgarza@gmail.com>
2026-08-20 01:43:45 +0800

    feat(stubs): restore Laurent polynomial matrix reduction

--- sage-stubs/matrix/matrix_laurent_mpolynomial_dense.pyi
+++ sage-stubs/matrix/matrix_laurent_mpolynomial_dense.pyi
@@ -1,26 +1,20 @@
-from collections.abc import Sequence
-from typing import Self
-
 from sage.matrix.matrix_generic_dense import Matrix_generic_dense
+from sage.matrix.matrix_mpolynomial_dense import Matrix_mpolynomial_dense
+from sage.rings.ideal import Ideal_generic
 from sage.rings.polynomial.laurent_polynomial_mpair import LaurentPolynomial_mpair
-from sage.rings.polynomial.polynomial_element import Polynomial
-from sage.structure.parent import ElementConstructorInput, Parent
 
 
 class Matrix_laurent_mpolynomial_dense(
     Matrix_generic_dense[LaurentPolynomial_mpair]
 ):
-    def __init__(
+    def laurent_matrix_reduction(
         self,
-        parent: Parent[Self],
-        entries: Sequence[LaurentPolynomial_mpair | ElementConstructorInput]
-        | ElementConstructorInput = ...,
-        copy: bool = ...,
-        coerce: bool = ...,
-    ) -> None: ...
-    def determinant(self, algorithm: str = ...) -> LaurentPolynomial_mpair: ...
-    det = determinant
-    def trace(self) -> LaurentPolynomial_mpair: ...
-    def characteristic_polynomial(self, var: str = ...) -> Polynomial: ...
-    charpoly = characteristic_polynomial
-    def monomial_denominator(self) -> LaurentPolynomial_mpair: ...
+    ) -> tuple[
+        Matrix_laurent_mpolynomial_dense,
+        Matrix_mpolynomial_dense,
+        Matrix_laurent_mpolynomial_dense,
+    ]: ...
+    def _fitting_ideal(
+        self,
+        i: int,
+    ) -> Ideal_generic: ...
