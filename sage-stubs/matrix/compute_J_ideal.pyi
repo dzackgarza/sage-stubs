f01deeb2d71650411b6d0ab18573b5cc0f47cd6e
D. Zack Garza <dzackgarza@gmail.com>
2026-08-19 23:09:16 +0800

    feat(stubs): specialize integer matrix null ideals

--- sage-stubs/matrix/compute_J_ideal.pyi
+++ sage-stubs/matrix/compute_J_ideal.pyi
@@ -1,60 +1,65 @@
 from collections.abc import Sequence
-from typing import Generic, TypeVar
 
 from sage.matrix.matrix import Matrix
 from sage.rings.fraction_field_element import FractionFieldElement
 from sage.rings.ideal import Ideal_generic
 from sage.rings.integer import Integer
 from sage.rings.polynomial.polynomial_element import Polynomial
-from sage.structure.element import RingElement
 from sage.structure.sage_object import SageObject
-
-_Scalar = TypeVar("_Scalar", bound=RingElement, default=RingElement)
 
 
 def lifting(
-    p: _Scalar,
+    p: Integer,
     t: int | Integer,
     A: Matrix[Polynomial],
     G: Matrix[Polynomial] | None,
 ) -> Matrix[Polynomial]: ...
 
 
-def p_part(f: Polynomial, p: _Scalar) -> Polynomial: ...
+def p_part(
+    f: Polynomial,
+    p: Integer,
+) -> Polynomial: ...
 
 
-class ComputeMinimalPolynomials(SageObject, Generic[_Scalar]):
+class ComputeMinimalPolynomials(SageObject):
     chi_B: Polynomial
     mu_B: Polynomial
 
-    def __init__(self, B: Matrix[_Scalar]) -> None: ...
+    def __init__(self, B: Matrix[Integer]) -> None: ...
     def find_monic_replacements(
         self,
-        p: _Scalar,
+        p: Integer,
         t: int | Integer,
         pt_generators: Sequence[Polynomial],
         prev_nu: Polynomial,
     ) -> list[Polynomial]: ...
     def current_nu(
         self,
-        p: _Scalar,
+        p: Integer,
         t: int | Integer,
         pt_generators: Sequence[Polynomial],
         prev_nu: Polynomial,
     ) -> Polynomial: ...
     def mccoy_column(
         self,
-        p: _Scalar,
+        p: Integer,
         t: int | Integer,
         nu: Polynomial,
     ) -> Matrix[Polynomial]: ...
     def p_minimal_polynomials(
         self,
-        p: _Scalar,
+        p: Integer,
         s_max: int | Integer | None = ...,
     ) -> dict[int, Polynomial]: ...
-    def null_ideal(self, b: _Scalar | int = ...) -> Ideal_generic: ...
-    def prime_candidates(self) -> list[_Scalar]: ...
+    def null_ideal(
+        self,
+        b: int | Integer = ...,
+    ) -> Ideal_generic: ...
+    def prime_candidates(self) -> list[Integer]: ...
     def integer_valued_polynomials_generators(
         self,
-    ) -> tuple[Polynomial, list[Polynomial | FractionFieldElement]]: ...
+    ) -> tuple[
+        Polynomial,
+        list[Polynomial | FractionFieldElement],
+    ]: ...
