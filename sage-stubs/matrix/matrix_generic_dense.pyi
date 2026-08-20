5a65c18436c1ef8594c8848d12dc8c2d8a9b3891
D. Zack Garza <dzackgarza@gmail.com>
2026-08-20 13:13:43 +0800

    feat(stubs): complete generic dense matrix storage

--- sage-stubs/matrix/matrix_generic_dense.pyi
+++ sage-stubs/matrix/matrix_generic_dense.pyi
@@ -1,12 +1,25 @@
+from collections.abc import Callable, Mapping, Sequence
 from typing import Generic, Literal, Self, TypeVar
 
 from sage.matrix.matrix import Matrix
 from sage.matrix.matrix_dense import Matrix_dense
-from sage.matrix.matrix_space import MatrixData
+from sage.matrix.matrix_space import MatrixData, MatrixSpace
+from sage.modules.free_module_element import FreeModuleElement
 from sage.structure.element import RingElement
-from sage.structure.parent import Parent
+from sage.structure.parent import ElementConstructorInput
 
 _Scalar = TypeVar("_Scalar", bound=RingElement, default=RingElement)
+
+
+type GenericDenseEntries[_T: RingElement] = (
+    Matrix[_T]
+    | Sequence[ElementConstructorInput]
+    | Sequence[Sequence[ElementConstructorInput]]
+    | Mapping[tuple[int, int], ElementConstructorInput]
+    | Callable[[int, int], ElementConstructorInput]
+    | MatrixData[_T]
+    | None
+)
 
 
 class Matrix_generic_dense(
@@ -15,8 +28,8 @@ class Matrix_generic_dense(
 ):
     def __init__(
         self,
-        parent: Parent[Self],
-        entries: MatrixData[_Scalar] = ...,
+        parent: MatrixSpace[_Scalar],
+        entries: GenericDenseEntries[_Scalar] = ...,
         copy: bool | None = ...,
         coerce: bool = ...,
     ) -> None: ...
@@ -28,6 +41,21 @@ class Matrix_generic_dense(
         version: int,
     ) -> None: ...
     def __copy__(self) -> Self: ...
+    def __bool__(self) -> bool: ...
+    def _list(self) -> list[_Scalar]: ...
+    def list(self) -> list[_Scalar]: ...
+    def _dict(self) -> dict[tuple[int, int], _Scalar]: ...
+    def dict(self, copy: bool = ...) -> dict[tuple[int, int], _Scalar]: ...
+    def row(
+        self,
+        i: int,
+        from_list: bool = ...,
+    ) -> FreeModuleElement[_Scalar]: ...
+    def column(
+        self,
+        j: int,
+        from_list: bool = ...,
+    ) -> FreeModuleElement[_Scalar]: ...
     def _add_(
         self,
         right: Matrix_generic_dense[_Scalar],
@@ -36,8 +64,20 @@ class Matrix_generic_dense(
         self,
         right: Matrix_generic_dense[_Scalar],
     ) -> Self: ...
+    def _lmul_(self, scalar: _Scalar) -> Self: ...
+    def _rmul_(self, scalar: _Scalar) -> Self: ...
+    def __neg__(self) -> Self: ...
     def _multiply_classical(
         self,
         right: Matrix[_Scalar],
-    ) -> Self: ...
-    def _list(self) -> list[_Scalar]: ...
+    ) -> Matrix[_Scalar]: ...
+    def transpose(self) -> Self: ...
+    T = transpose
+    def antitranspose(self) -> Self: ...
+    def randomize(
+        self,
+        density: float = ...,
+        nonzero: bool = ...,
+        *args: object,
+        **kwds: object,
+    ) -> None: ...
