b1e98b1d7cc3641d6dd3c98c607e87c7ca4d5104
D. Zack Garza <dzackgarza@gmail.com>
2026-08-20 12:57:23 +0800

    feat(stubs): type SIDH curves, keys, and isogeny paths

--- sage-stubs/crypto/public_key/key_exchange/sidh.pyi
+++ sage-stubs/crypto/public_key/key_exchange/sidh.pyi
@@ -1,21 +1,84 @@
-# Generated from the pinned Sage 10.7 source tree.
-import builtins
-from collections.abc import AsyncIterator as _AsyncIterator, Iterable as _Iterable, Iterator as _Iterator
 from typing import Self
 
-class _SageObject: ...
+from sage.crypto.public_key.key_exchange.key_exchange_base import KeyExchangeBase
+from sage.rings.finite_rings.element_base import FinitePolyExtElement
+from sage.rings.integer import Integer
+from sage.schemes.elliptic_curves.ell_finite_field import (
+    EllipticCurve_finite_field,
+)
+from sage.schemes.elliptic_curves.ell_point import (
+    EllipticCurvePoint_finite_field,
+)
+from sage.schemes.elliptic_curves.hom_composite import (
+    EllipticCurveHom_composite,
+)
 
-class SIDH:
-    def __init__(self, E: builtins.object, PA: builtins.object, QA: builtins.object, PB: builtins.object, QB: builtins.object) -> None: ...
+
+type SecretKeySIDH = int | Integer
+type PublicKeySIDH = tuple[
+    EllipticCurve_finite_field,
+    EllipticCurvePoint_finite_field,
+    EllipticCurvePoint_finite_field,
+]
+type SIDHParameters = tuple[
+    Integer,
+    EllipticCurve_finite_field,
+    EllipticCurvePoint_finite_field,
+    EllipticCurvePoint_finite_field,
+    EllipticCurvePoint_finite_field,
+    EllipticCurvePoint_finite_field,
+]
+
+
+class SIDH(
+    KeyExchangeBase[
+        Integer,
+        PublicKeySIDH,
+        Integer,
+        PublicKeySIDH,
+        FinitePolyExtElement,
+    ],
+):
+    def __init__(
+        self,
+        E: EllipticCurve_finite_field,
+        PA: EllipticCurvePoint_finite_field,
+        QA: EllipticCurvePoint_finite_field,
+        PB: EllipticCurvePoint_finite_field,
+        QB: EllipticCurvePoint_finite_field,
+    ) -> None: ...
     @classmethod
-    def parameter_set_from_prime(cls, prime: builtins.object) -> Self: ...
+    def parameter_set_from_prime(
+        cls,
+        prime: int | Integer,
+    ) -> Self: ...
     @classmethod
-    def named_parameter_set(cls, name: builtins.str) -> Self: ...
-    def parameters(self) -> _SageObject: ...
-    def alice_secret_key(self) -> _SageObject: ...
-    def bob_secret_key(self) -> _SageObject: ...
-    def alice_public_key(self, alice_secret_key: builtins.object) -> _SageObject: ...
-    def bob_public_key(self, bob_secret_key: builtins.object) -> _SageObject: ...
-    def alice_compute_shared_secret(self, alice_secret_key: builtins.object, bob_public_key: builtins.object) -> _SageObject: ...
-    def bob_compute_shared_secret(self, bob_secret_key: builtins.object, alice_public_key: builtins.object) -> _SageObject: ...
-    def secret_isogeny_path(self, start_curve: builtins.object, secret_key: builtins.object, P: builtins.int, Q: builtins.int) -> _SageObject: ...
+    def named_parameter_set(cls, name: str) -> Self: ...
+    def parameters(self) -> SIDHParameters: ...
+    def alice_secret_key(self) -> Integer: ...
+    def bob_secret_key(self) -> Integer: ...
+    def alice_public_key(
+        self,
+        alice_secret_key: SecretKeySIDH,
+    ) -> PublicKeySIDH: ...
+    def bob_public_key(
+        self,
+        bob_secret_key: SecretKeySIDH,
+    ) -> PublicKeySIDH: ...
+    def alice_compute_shared_secret(
+        self,
+        alice_secret_key: SecretKeySIDH,
+        bob_public_key: PublicKeySIDH,
+    ) -> FinitePolyExtElement: ...
+    def bob_compute_shared_secret(
+        self,
+        bob_secret_key: SecretKeySIDH,
+        alice_public_key: PublicKeySIDH,
+    ) -> FinitePolyExtElement: ...
+    def secret_isogeny_path(
+        self,
+        start_curve: EllipticCurve_finite_field,
+        secret_key: SecretKeySIDH,
+        P: EllipticCurvePoint_finite_field,
+        Q: EllipticCurvePoint_finite_field,
+    ) -> tuple[EllipticCurveHom_composite, list[Integer]]: ...
