dc536876eee1b8b01a0cd43aa838388486ad68bf
D. Zack Garza <dzackgarza@gmail.com>
2026-08-24 04:53:31 +0800

    feat(stubs): restore complex-double vector specialization

--- sage-stubs/modules/vector_complex_double_dense.pyi
+++ sage-stubs/modules/vector_complex_double_dense.pyi
@@ -1,41 +1,41 @@
-from collections.abc import Sequence
-from typing import Self
+from collections.abc import Callable, Sequence
 
-from sage.matrix.matrix_complex_double_dense import Matrix_complex_double_dense
+from sage.modules.free_module import FreeModule_generic, ModuleRank
 from sage.modules.vector_double_dense import Vector_double_dense
 from sage.rings.complex_double import ComplexDoubleElement
-from sage.rings.real_double import RealDoubleElement
 from sage.structure.parent import ElementConstructorInput
 
 
-class Vector_complex_double_dense(
-    Vector_double_dense[ComplexDoubleElement]
-):
-    def __init__(
+class Vector_complex_double_dense(Vector_double_dense[ComplexDoubleElement]):
+    def __reduce__(
         self,
-        parent: object,
-        entries: Sequence[complex | ComplexDoubleElement | ElementConstructorInput] = ...,
-        coerce: bool = ...,
-        copy: bool = ...,
-    ) -> None: ...
-    def __copy__(self) -> Self: ...
-    def list(self, copy: bool = ...) -> list[ComplexDoubleElement]: ...
-    def dot_product(
-        self,
-        right: Vector_complex_double_dense,
-    ) -> ComplexDoubleElement: ...
-    def hermitian_inner_product(
-        self,
-        right: Vector_complex_double_dense,
-    ) -> ComplexDoubleElement: ...
-    inner_product = hermitian_inner_product
-    def pairwise_product(self, right: Vector_complex_double_dense) -> Self: ...
-    def norm(self, p: int | float | str = ...) -> float: ...
-    def conjugate(self) -> Self: ...
-    def real(self) -> Vector_real_double_dense: ...
-    def imag(self) -> Vector_real_double_dense: ...
-    def row(self) -> Matrix_complex_double_dense: ...
-    def column(self) -> Matrix_complex_double_dense: ...
+    ) -> tuple[
+        Callable[
+            [
+                FreeModule_generic[ComplexDoubleElement],
+                Sequence[ElementConstructorInput],
+                ModuleRank,
+                bool | None,
+            ],
+            Vector_complex_double_dense,
+        ],
+        tuple[
+            FreeModule_generic[ComplexDoubleElement],
+            list[ComplexDoubleElement],
+            int,
+            bool,
+        ],
+    ]: ...
 
 
-from sage.modules.vector_real_double_dense import Vector_real_double_dense
+def unpickle_v0(
+    parent: FreeModule_generic[ComplexDoubleElement],
+    entries: Sequence[ElementConstructorInput],
+    degree: ModuleRank,
+) -> Vector_complex_double_dense: ...
+def unpickle_v1(
+    parent: FreeModule_generic[ComplexDoubleElement],
+    entries: Sequence[ElementConstructorInput],
+    degree: ModuleRank,
+    immutable: bool | None = ...,
+) -> Vector_complex_double_dense: ...
