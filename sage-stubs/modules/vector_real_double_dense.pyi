84d0f3caeef19371a9c5783580a0f1f5333f4265
D. Zack Garza <dzackgarza@gmail.com>
2026-08-24 04:53:23 +0800

    feat(stubs): restore real-double vector specialization

--- sage-stubs/modules/vector_real_double_dense.pyi
+++ sage-stubs/modules/vector_real_double_dense.pyi
@@ -1,25 +1,42 @@
-from collections.abc import Sequence
-from typing import Self
+from collections.abc import Callable, Sequence
 
-from sage.matrix.matrix_real_double_dense import Matrix_real_double_dense
+from sage.modules.free_module import FreeModule_generic, ModuleRank
 from sage.modules.vector_double_dense import Vector_double_dense
 from sage.rings.real_double import RealDoubleElement
 from sage.structure.parent import ElementConstructorInput
 
 
 class Vector_real_double_dense(Vector_double_dense[RealDoubleElement]):
-    def __init__(
+    def stats_skew(self, bias: bool = ...) -> RealDoubleElement: ...
+    def __reduce__(
         self,
-        parent: object,
-        entries: Sequence[float | RealDoubleElement | ElementConstructorInput] = ...,
-        coerce: bool = ...,
-        copy: bool = ...,
-    ) -> None: ...
-    def __copy__(self) -> Self: ...
-    def list(self, copy: bool = ...) -> list[RealDoubleElement]: ...
-    def dot_product(self, right: Vector_real_double_dense) -> RealDoubleElement: ...
-    inner_product = dot_product
-    def pairwise_product(self, right: Vector_real_double_dense) -> Self: ...
-    def norm(self, p: int | float | str = ...) -> float: ...
-    def row(self) -> Matrix_real_double_dense: ...
-    def column(self) -> Matrix_real_double_dense: ...
+    ) -> tuple[
+        Callable[
+            [
+                FreeModule_generic[RealDoubleElement],
+                Sequence[ElementConstructorInput],
+                ModuleRank,
+                bool | None,
+            ],
+            Vector_real_double_dense,
+        ],
+        tuple[
+            FreeModule_generic[RealDoubleElement],
+            list[RealDoubleElement],
+            int,
+            bool,
+        ],
+    ]: ...
+
+
+def unpickle_v0(
+    parent: FreeModule_generic[RealDoubleElement],
+    entries: Sequence[ElementConstructorInput],
+    degree: ModuleRank,
+) -> Vector_real_double_dense: ...
+def unpickle_v1(
+    parent: FreeModule_generic[RealDoubleElement],
+    entries: Sequence[ElementConstructorInput],
+    degree: ModuleRank,
+    immutable: bool | None = ...,
+) -> Vector_real_double_dense: ...
